e9c5e9af28180920e57763d2bd7da5ff3f87d234
Nathan Lane <lanenathan214@gmail.com>
2024-07-04 10:53:13 -0400

    added a table for product tags and deleted inserts

--- db/schema.sql
+++ db/schema.sql
@@ -26,49 +26,17 @@ CREATE TABLE products (
 CREATE TABLE tags (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     product_id INT REFERENCES products(id),
-    tag_id INT REFERENCES tags(id)
+    tag_name VARCHAR(30) NOT NULL
 );
 
---INSERT INTO TABLES
+CREATE TABLE productTag(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT ,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
 
-INSERT INTO categories (category_name) VALUES ('Clothing');
-('Shirts'),
-('Pants'),
-('Shoes'),
-('Hats'),
-('Jewelry');
-
-INSERT INTO products (product_name, price, stock, category_id) VALUES ('T-shirt', 15.00, 14, 1),
-('Polo', 20.00, 12, 1),
-('Dress Pants', 35.00, 5, 2),
-('Jeans', 40.00, 8, 2),
-('Sneakers', 50.00, 10, 3),
-('Dress Shoes', 60.00, 6, 3),
-('Baseball Cap', 15.00, 22, 4),
-('Beanie', 10.00, 18, 4),
-('Necklace', 25.00, 10, 5),
-('Bracelet', 20.00, 15, 5);
-
-INSERT INTO tags (product_id, tag_id) VALUES (1, 1),
-(1, 2),
-(2, 1),
-(2, 2),
-(3, 3),
-(3, 4),
-(4, 3),
-(4, 4),
-(5, 5),
-(5, 6),
-(6, 5),
-(6, 6),
-(7, 7),
-(7, 8),
-(8, 7),
-(8, 8),
-(9, 9),
-(9, 10),
-(10, 9),
-(10, 10);
 
 
 
